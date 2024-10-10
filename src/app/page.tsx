474043f32a88c6a2254371860d6801c19169da82
Keith Center <keith@centerjustified.com>
2024-10-10 15:21:24 -0400

    Lint fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import { api, HydrateClient } from "@/trpc/server";
+import { HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
   // const hello = await api.post.hello({ text: "from tRPC" });
