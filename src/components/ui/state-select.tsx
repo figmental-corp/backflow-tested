5ae6e05f103db429b1af63c2d734328a771b5b03
Keith Center <keith@centerjustified.com>
2024-10-16 00:20:37 -0400

    Refactored state select component

--- src/components/ui/state-select.tsx
+++ src/components/ui/state-select.tsx
@@ -7,76 +7,43 @@ import {
   SelectContent,
 } from "./select";
 
-const states = [
-  { value: "AL", label: "Alabama" },
-  { value: "AK", label: "Alaska" },
-  { value: "AZ", label: "Arizona" },
-  { value: "AR", label: "Arkansas" },
-  { value: "CA", label: "California" },
-  { value: "CO", label: "Colorado" },
-  { value: "CT", label: "Connecticut" },
-  { value: "DE", label: "Delaware" },
-  { value: "FL", label: "Florida" },
-  { value: "GA", label: "Georgia" },
-  { value: "HI", label: "Hawaii" },
-  { value: "ID", label: "Idaho" },
-  { value: "IL", label: "Illinois" },
-  { value: "IN", label: "Indiana" },
-  { value: "IA", label: "Iowa" },
-  { value: "KS", label: "Kansas" },
-  { value: "KY", label: "Kentucky" },
-  { value: "LA", label: "Louisiana" },
-  { value: "ME", label: "Maine" },
-  { value: "MD", label: "Maryland" },
-  { value: "MA", label: "Massachusetts" },
-  { value: "MI", label: "Michigan" },
-  { value: "MN", label: "Minnesota" },
-  { value: "MS", label: "Mississippi" },
-  { value: "MO", label: "Missouri" },
-  { value: "MT", label: "Montana" },
-  { value: "NE", label: "Nebraska" },
-  { value: "NV", label: "Nevada" },
-  { value: "NH", label: "New Hampshire" },
-  { value: "NJ", label: "New Jersey" },
-  { value: "NM", label: "New Mexico" },
-  { value: "NY", label: "New York" },
-  { value: "NC", label: "North Carolina" },
-  { value: "ND", label: "North Dakota" },
-  { value: "OH", label: "Ohio" },
-  { value: "OK", label: "Oklahoma" },
-  { value: "OR", label: "Oregon" },
-  { value: "PA", label: "Pennsylvania" },
-  { value: "RI", label: "Rhode Island" },
-  { value: "SC", label: "South Carolina" },
-  { value: "SD", label: "South Dakota" },
-  { value: "TN", label: "Tennessee" },
-  { value: "TX", label: "Texas" },
-  { value: "UT", label: "Utah" },
-  { value: "VT", label: "Vermont" },
-  { value: "VA", label: "Virginia" },
-  { value: "WA", label: "Washington" },
-  { value: "WV", label: "West Virginia" },
-  { value: "WI", label: "Wisconsin" },
-  { value: "WY", label: "Wyoming" },
-];
+import states from "@/constants/states";
+import { Label } from "@radix-ui/react-label";
+import { Controller } from "react-hook-form";
 
-const StateSelect = React.forwardRef<
-  HTMLDivElement,
-  React.ComponentPropsWithoutRef<typeof Select>
->(({ ...props }) => (
-  <Select {...props}>
-    <SelectTrigger>
-      <SelectValue placeholder="Select a state" />
-    </SelectTrigger>
-    <SelectContent>
-      {states.map((state) => (
-        <SelectItem key={state.value} value={state.value}>
-          {state.label}
-        </SelectItem>
-      ))}
-    </SelectContent>
-  </Select>
-));
+interface StateSelectProps
+  extends React.ComponentPropsWithoutRef<typeof Select> {
+  control: any;
+  name?: string;
+  errors: any;
+}
+
+const StateSelect = React.forwardRef<HTMLDivElement, StateSelectProps>(
+  ({ control, name = "state", errors }) => (
+    <>
+      <Label htmlFor={name}>State</Label>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <Select onValueChange={field.onChange} value={field.value}>
+            <SelectTrigger>
+              <SelectValue placeholder="Select a state" />
+            </SelectTrigger>
+            <SelectContent>
+              {states.map((state) => (
+                <SelectItem key={state.value} value={state.value}>
+                  {state.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        )}
+      />
+      <p className="min-h-5 text-sm text-red-500">{errors.state?.message}</p>
+    </>
+  ),
+);
 
 StateSelect.displayName = "StateSelect";
 
